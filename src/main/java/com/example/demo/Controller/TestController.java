ee6af6af41ecc5dbcf7b9cfbd7c985afa237866a
shyboy0358 <shyboy0358@hotmail.com>
2019-07-13 17:57:35 +0800

    init

---
+++ src/main/java/com/example/demo/Controller/TestController.java
@@ -0,0 +1,12 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+
+//@Controller
+public class TestController {
+
+    TestController(){
+        System.out.println("init");
+    }
+
+}
